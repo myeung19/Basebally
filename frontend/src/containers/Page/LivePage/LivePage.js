c85cabeb84ddac9a7a4b014a110d44346c81261a
Lay <tonglay1219@gmail.com>
2018-11-23 20:14:39 -0700

    Separate each page to a single file

---
+++ frontend/src/containers/Page/LivePage/LivePage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import CustomPaper from "../../../components/CustomPaper/CustomPaper";
+
+class LivePage extends Component {
+    render() {
+        return (
+            <CustomPaper value="LivePage"/>
+        );
+    }
+}
+
+export default LivePage;
