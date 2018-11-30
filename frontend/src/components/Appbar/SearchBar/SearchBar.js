9d8ab538e225caf42d448b880dea54ec4ac1f7da
Lay <tonglay1219@gmail.com>
2018-11-30 12:44:35 -0700

    Progress on the search page - Add the search text field and an icon button next to it

--- frontend/src/components/Appbar/SearchBar/SearchBar.js
+++ frontend/src/components/Appbar/SearchBar/SearchBar.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import InputBase from '@material-ui/core/InputBase';
+
 import "./SearchBar.css"
+import IconButton from "@material-ui/core/es/IconButton/IconButton";
+import SearchIcon from '@material-ui/icons/Search';
 
 const searchBar = props => {
     return (
-        <div>
-
+        <div className="SearchBar">
+            <InputBase
+                className="searchTextField"
+                placeholder="firstname, then lastname"
+            />
+            <div className="searchBtn">
+                <IconButton>
+                    <SearchIcon/>
+                </IconButton>
+            </div>
         </div>
     );
 };
