ab36efc0cd2a351534d5c26972f7079385d29d51
Lay <tonglay1219@gmail.com>
2018-12-04 13:15:53 -0700

    Add icon to asset folder

--- frontend/desktop/public/electron.js
+++ frontend/desktop/public/electron.js
@@ -12,7 +12,8 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 900,
         height: 680,
-        title: "Basebally"
+        title: "Basebally",
+        icon: path.join(__dirname, '../src/assets/icons/png/icon_32@2x.png')
     });
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
     mainWindow.on('closed', () => mainWindow = null);
